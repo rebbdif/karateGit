4c64c6fc426932ba4c55b50033c870bcc9b9c8f7
Леонид Серебряный <leosilver@yandex.ru>
2018-08-06 23:20:56 +0300

    лод

---
+++ karateGit/ExtraClass.swift
@@ -0,0 +1,13 @@
+//
+//  ExtraClass.swift
+//  karateGit
+//
+//  Created by Леонид Серебряный on 06/08/2018.
+//  Copyright © 2018 Леонид Серебряный. All rights reserved.
+//
+
+import Cocoa
+
+class ExtraClass: NSObject {
+
+}
