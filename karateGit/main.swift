fe977bd1ad5e0374c5f195fcd6f57ae3cfd3822c
Леонид Серебряный <leosilver@yandex.ru>
2018-08-06 23:02:04 +0300

    начал проект

---
+++ karateGit/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  karateGit
+//
+//  Created by Леонид Серебряный on 06/08/2018.
+//  Copyright © 2018 Леонид Серебряный. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
