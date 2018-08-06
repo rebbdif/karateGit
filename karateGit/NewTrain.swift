7a801bbe39fd4f923da38b79df1804237f800941
Леонид Серебряный <leosilver@yandex.ru>
2018-08-06 23:40:12 +0300

    dsf

--- karateGit/NewTrain.swift
+++ karateGit/NewTrain.swift
@@ -1,13 +1,12 @@
 //
-//  ExtraClass.swift
+//  NewTrain.swift
 //  karateGit
 //
 //  Created by Леонид Серебряный on 06/08/2018.
 //  Copyright © 2018 Леонид Серебряный. All rights reserved.
 //
 
-import Cocoa
 
-class ExtraClass: NSObject {
-
+class NewTrain {
+    var name = "dsfdsaf"
 }
